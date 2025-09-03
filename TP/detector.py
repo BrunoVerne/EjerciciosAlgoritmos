5bfc7d68be590a42f1e2f4ffd3f3a1f0e15db4ff
BrunoVerne <bverne@fi.uba.ar>
2025-09-03 15:41:12 -0300

    logaritmo a chequear del segundo punto

---
+++ TP/detector.py
@@ -0,0 +1,74 @@
+import random
+
+class Detector:
+    def __init__(self, confiable=True, id=None):
+        self.confiable = confiable
+        self.id = id  
+    
+    def funciona(self, detector_a_probar):
+        if self.confiable:  
+            return detector_a_probar.confiable
+        else:
+            return False
+    
+    def __repr__(self):
+        return f"Detector({self.id})"
+
+
+
+
+def detector_de_mentiras(vector, inicio, fin):
+    if(fin - inicio == 0):
+        return vector[inicio]
+    elif(fin - inicio < 0):
+        return None
+    
+    elif (fin - inicio == 1):
+        if(vector[inicio].funciona(vector[fin]) and vector[fin].funciona(vector[inicio])):
+            return vector[inicio]
+        return None
+    
+    mitad = (inicio + fin) //2
+    
+    candidato = detector_de_mentiras(vector, inicio, mitad)
+    if candidato is not None:
+        if(chequearMayoria(candidato,vector)):
+            return candidato
+    
+    candidato = detector_de_mentiras(vector, mitad + 1, fin)
+    if candidato is not None:
+        if(chequearMayoria(candidato,vector)):
+            return candidato
+        
+    return None
+    
+    
+
+
+def chequearMayoria(elemento, vector):
+    contador = 0
+    for i in vector:
+        if i.funciona(elemento) and elemento.funciona(i):
+            contador +=1
+    return contador > len(vector) // 2
+
+if __name__ == "__main__":
+    dA = Detector(True, "A")
+    dB = Detector(True, "B")
+    dC = Detector(True, "C")
+    dD = Detector(False, "D")
+    dE = Detector(False, "E")
+    dF = Detector(True, "F")
+    dG = Detector(False, "G")
+    
+    vector1 = [dA, dB, dC, dD]      # mayoría confiable -> devuelve un confiable
+    vector2 = [dA, dB, dD, dE]       # 2 confiables de 4 -> no hay mayoría -> None
+    vector3 = [dA, dB, dC, dF]       # 4 confiables -> devuelve un confiable
+    vector4 = [dD, dE, dG]            # ninguno confiable -> None
+    vector5 = [dA, dD, dF, dG, dB]     # 3 confiables de 5 -> devuelve un confiable
+
+    print(detector_de_mentiras(vector1, 0, len(vector1)-1))
+    print(detector_de_mentiras(vector2,0, len(vector2)-1))
+    print(detector_de_mentiras(vector3, 0, len(vector3)-1))
+    print(detector_de_mentiras(vector4, 0, len(vector4)-1))
+    print(detector_de_mentiras(vector5, 0, len(vector5)-1))    
