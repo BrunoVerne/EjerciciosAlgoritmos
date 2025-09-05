b65959b7f19d996ad4db3ca9ba70d8a5dc8d154d
BrunoVerne <bverne@fi.uba.ar>
2025-09-04 21:23:00 -0300

    apendice de ejericio de invitaciones con cola

--- Greedy/invitaciones.py
+++ Greedy/invitaciones.py
@@ -17,7 +17,7 @@ def seleccionar_invitados(n):
     cola = set()
 
     for s in n:
-        if len(s.conocidos ) < 4: 
+        if s.conocidos < 4: 
             n.remove(s)
             for conocido in s.conocidos:
                 cola.add(conocido)
@@ -37,10 +37,71 @@ def seleccionar_invitados(n):
 
     
     for socioAinvitar in n:
-        socioAinvitar.invitar()
+        socioAinvitar.invitar
 
 
 
 def invitar(self):
     print(f"{self.nombre} ha sido invitado.")
 
+
+
+
+# Considere el problema anterior, pero con la adición de una nueva restricción: Los
+# organizadores desean que cada invitado pueda conocer nuevas personas. Por lo que nos
+# solicitan que sólo puede asistir si NO conoce al menos otras 4 personas invitadas. Modiﬁque
+# su propuesta para satisfacer esta nueva solución.
+
+
+def seleccionar_invitados2(invitados):
+    revisar = set()
+    desinvitados = set()
+
+    for s in invitados:
+        
+                
+        if len(invitados) - len(s.conocidos) < 4: # desconocidos = cantidad de invitados - conocidos
+            invitados.remove(s)
+            desinvitados.add(s)
+            
+            for conocido in s.conocidos:
+                if conocido in desinvitados:
+                    conocido.conocidos.remove(s)
+                    revisar.add(conocido)       
+        
+
+    seAgrego = True
+    while seAgrego:
+        seAgrego = False
+        
+        invitados = len(invitados)
+        
+        for c in revisar:
+
+            if len(invitados) - len(c.conocidos) >= 4:
+                invitados.add(c)
+                desinvitados.remove(c)
+                revisar.remove(c)
+                seAgrego = True
+                
+                for conocido in c.conocidos:
+                    if conocido in invitados:
+                        conocido.conocidos.add(c) # porque acabo de meter a un conocido a la lista
+                        revisar.add(conocido)         
+            else:
+                revisar.remove(c)
+                for conocido in c.conocidos:
+                    if conocido in desinvitados:
+                        conocido.conocidos.remove(c)
+                        revisar.add(conocido)
+
+
+            
+    for socioAinvitar in invitados:
+        socioAinvitar.invitar                                    
+
+    
+
+
+                
+    
