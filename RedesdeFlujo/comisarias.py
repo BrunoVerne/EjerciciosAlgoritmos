2a120b9b08e75cd41175ba9908e3f431441923ae
BrunoVerne <bverne@fi.uba.ar>
2025-10-21 23:15:37 -0300

    salen dos mas

---
+++ RedesdeFlujo/comisarias.py
@@ -0,0 +1,21 @@
+# La policía de la ciudad tiene “n” comisarías dispersas por la ciudad. Para un evento
+# deportivo internacional deben asignar la custodia de “m” centros de actividades. Una
+# comisaría y un centro de actividades pueden ser emparejados si y sólo si la distancia entre
+# ellos no es mayor a un valor d. Contamos con la distancia entre todos los centros y las
+# comisarías. Una comisaría sólo puede custodiar un centro. El centro puede ser custodiado
+# por una comisaría. Determinar si es posible la asignación de tal forma que todos los centros
+# estén custodiados. 
+
+
+
+
+
+
+
+
+
+#Cómo modiﬁcaría la resolución del problema si en lugar de que cada
+# centro de actividades i tenga que ser asignado a una sola comisaría, tenga que ser asignado
+# a mi comisarías? ¿Cómo modiﬁcaría la resolución del problema si además hubiera una
+# restricción entre comisarías que implicaría que una comisaría Ni y una Nj no pudieran ser
+# asignadas juntas a un centro Mi? ¿Para qué casos dejaría de ser eﬁciente la resolución?
